1bfb11f976bc57ecb11d23c6600dfc2be2136940
Rafał Kuligowski <rafal.kuligowski87@gmail.com>
2018-04-05 19:02:50 +0100

    nowy kompoment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NewHabitComponent } from './new-habit/new-habit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewHabitComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/new-habit/new-habit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-habit',
+  templateUrl: './new-habit.component.html',
+  styleUrls: ['./new-habit.component.css']
+})
+export class NewHabitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
