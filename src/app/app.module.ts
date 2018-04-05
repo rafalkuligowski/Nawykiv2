836ed320c64bd79485a56a745b652c9e6b0a6add
Rafał Kuligowski <rafal.kuligowski87@gmail.com>
2018-04-05 19:27:28 +0100

    input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NewHabitComponent } from './new-habit/new-habit.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { NewHabitComponent } from './new-habit/new-habit.component';
     NewHabitComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/new-habit/new-habit.component.ts
+++ src/app/new-habit/new-habit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-habit',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-habit.component.css']
 })
 export class NewHabitComponent implements OnInit {
-
+  newHabit: string;
   constructor() { }
 
   ngOnInit() {
